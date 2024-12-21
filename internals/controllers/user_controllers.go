1ed16b14a7d2109430b58bc5611565e3baacbf4e
suhaasya <suhaskhobragade19@gmail.com>
2024-12-21 11:14:37 +0530

    feat: adds completed code

--- internals/controllers/user_controllers.go
+++ internals/controllers/user_controllers.go
@@ -1,18 +1,75 @@
 package controllers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"simple-go-server/pkg/helper"
+	"simple-go-server/pkg/structs"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request){
-	
+	reqData := new(structs.AddUser)
+	err := helper.ValidateBody(r, reqData)
+
+	if err != nil {	
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(reqData.Email)
+	fmt.Println(reqData.Name)
+	fmt.Println(reqData.Password)
+	fmt.Println(reqData.Role)
+
  	w.Write([]byte("User created"))	
 }
 
 func EditUser(w http.ResponseWriter, r *http.Request){
-	
+	reqData := new(structs.EditUser)
+	err := helper.ValidateBody(r, reqData)
+
+	if err != nil {	
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if reqData.Email != "" {
+		fmt.Println(reqData.Email)
+	}
+	if reqData.Name != "" {
+		fmt.Println(reqData.Name)
+	}
+	if reqData.Role != "" {
+		fmt.Println(reqData.Role)
+	}
+
  	w.Write([]byte("User edited"))	
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request){
+	query := r.URL.Query()
+	queryJson, _ := helper.QueryToJSON(query)
+	queryParams := new(structs.QueryParams)
+
+	err := helper.ValidateQueryParams([]byte(queryJson), queryParams)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if queryParams.Page != "" {
+		fmt.Println(queryParams.Page)
+	}
+	if queryParams.Order != "" {
+		fmt.Println(queryParams.Order)
+	}
+	if queryParams.OrderBy != "" {
+		fmt.Println(queryParams.OrderBy)
+	}
+	if queryParams.Size != "" {
+		fmt.Println(queryParams.Size)
+	}
 
  	w.Write([]byte("User fetched"))	
 }
--- pkg/structs/user.go
+++ pkg/structs/user.go
@@ -1,1 +1,21 @@
 package structs
+
+type AddUser struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+	Role     string `json:"role" validate:"required,oneof=admin user"`
+}
+
+type EditUser struct {
+	Name     string `json:"name" validate:"omitempty"`
+	Email    string `json:"email" validate:"omitempty,email"`
+	Role     string `json:"role" validate:"omitempty,oneof=admin user"`
+}
+
+type QueryParams struct {
+	Page    string `json:"page" validate:"omitempty,numeric,min=1"`
+	Size    string `json:"size" validate:"omitempty,numeric,min=1"`
+	Order   string `json:"order" validate:"omitempty,oneof=asc desc"`
+	OrderBy string `json:"orderBy" validate:"omitempty,oneof=id name"`
+}
